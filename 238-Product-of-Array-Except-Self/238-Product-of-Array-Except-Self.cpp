19fbcddbd55c4f097f64872e4941c4ce79ba9899
devxml <devverma2031@gmail.com>
2026-06-28 14:47:51 +0530

    Solution Added - LeetCode75 to GitHub

--- 238-Product-of-Array-Except-Self/238-Product-of-Array-Except-Self.cpp
+++ 238-Product-of-Array-Except-Self/238-Product-of-Array-Except-Self.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> answer;
+        vector<int> ans(n, 1);
 
+        int prefix = 1;
         for (int i = 0; i < n; i++) {
-            int mult = 1;
-
-            for (int j = 0; j < n; j++) {
-                if (j == i)
-                    continue;
-
-                mult *= nums[j];
-            }
-
-            answer.push_back(mult);
+            ans[i] = prefix;
+            prefix *= nums[i];
         }
-
-        return answer;
+        
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] *= suffix;
+            suffix *= nums[i];
+        }
+        
+        return ans;
     }
 };
